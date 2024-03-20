37f27e123ebae53749f197fe03af4e6c053a0bc5
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-03-19 23:24:45 -0600

    adjusted methods to better reuse examples and fit theme

--- src/solved/SRP/EmailService.java
+++
@@ -1,8 +0,0 @@
-package solved.SRP;
-
-public class EmailService {
-    //send notification email
-    public void sendNotificationEmail(String email, String message) {
-        System.out.println("Sending email to " + email + ": " + message);
-    }
-}
